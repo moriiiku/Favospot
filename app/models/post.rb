723866b6d9a26873d1311d56bdf2ab6622d47609
moriiiku <mo.1221.mrk@gmail.com>
2020-07-09 13:34:13 +0000

    add date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	has_many :favorites, dependent: :destroy
 	has_many :post_images, dependent: :destroy
+
 
 	accepts_nested_attributes_for :post_images
 	#?
+
+	with_options presence: true do
+		validates :title, presence: { message: "を入力してください。"}
+		validates :body, presence: { message: "はなぜお気に入りなのか入力してください。"}
+
+		#validates :place
+
+		#検証
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
-  
-
   has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+
+  validates :user_name, presence: true
+
+  def already_favorited?(post)
+    self.favorites.exists?(post_id: post.id)
+  end
+
+
 end
